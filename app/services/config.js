baf9e1de0381fa0421afde1a2a97ef09846570e6
ccoenraets <ccoenraets@gmail.com>
2016-02-04 11:07:15 -0500

    adding server url detection

--- app/services/config.js
+++ app/services/config.js
@@ -1,8 +1,7 @@
 function isHeroku()
 {
-    // Rudimentary check to see if the app is running on Heroku. assumes you don't have a PORT env var set locally
-    return process.env.PORT;
+    // Rudimentary check to see if we are running on Heroku. Should provide a more flexible config.
+    return window.location.indexOf('herokuapp') > 0;
 }
-
 
 export let SERVER_URL = isHeroku() ? "/" : "http://localhost:5000/";
